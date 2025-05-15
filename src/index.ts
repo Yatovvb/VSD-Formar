2697d17863b96a05875d775fb70a37e5a0ac5169
Yatovvb <voldigoadb@gmail.com>
2025-05-15 00:01:27 +0000

    cadastro usuario

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { mainModule } from "process";
+import { sequelize } from "./config/database";
+import User from "./models/User";
 
 const app = express();
 const port = 3000;
@@ -7,4 +10,19 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+main()
+
+async function main() {
+  if (require.main === module) {
+    await sequelize.sync({ force: true}).then(() => {
+      console.log("Database is synced");
+        app.listen(port, () =>
+          console.log('listening at https://localhost:$(port'),
+    
+      );
+});
+
+//criar usuario
+const user = await User.create({ name: 'Alice', email: 'alice@gmail.com'});
+console.log("Usuario criado:", user.toJSON());
+  }}
